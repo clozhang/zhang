15af4d6a785ca4395d0cfd0ae0925d94e2c1a68c
Duncan McGreggor <oubiwann@gmail.com>
2016-02-13 12:15:26 -0600

    Added initial files.

---
+++ README.md
@@ -0,0 +1,13 @@
+# zhang
+
+*An implementation of the Erlang process model in Clojure core.async*
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2016 Duncan McGreggor
+
+Distributed under the Apache License Version 2.0.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject zhang "0.1.0-dev"
+  :description "An implementation of the Erlang process model in Clojure
+                core.async"
+  :url "https://github.com/oubiwann/zhang"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
