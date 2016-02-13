6d6678458d82b2bf097b52093d4d59a204adaf9e
Duncan McGreggor <oubiwann@gmail.com>
2016-02-13 15:02:20 -0600

    Updated deps and added codox support.

--- project.clj
+++ project.clj
@@ -4,4 +4,18 @@
   :url "https://github.com/oubiwann/zhang"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [dire "0.5.4"]
+                 [twig "0.1.2"]]
+  :plugins [[lein-codox "0.9.4"]
+            [lein-simpleton "1.3.0"]]
+  :repl-options {:init-ns zhang.dev}
+  :test-selectors {:default :unit
+                   :unit :unit
+                   :system :system
+                   :integration :integration}
+  :codox {:output-path "docs/master/current"
+          :doc-paths ["docs/source"]
+          :namespaces [#"^zhang\.(?!test)"]
+          :metadata {:doc/format :markdown}})
