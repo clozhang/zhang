d3eb766bfc9e37a69fe9589f037ceb1053d9b79a
Duncan McGreggor <oubiwann@gmail.com>
2017-01-07 19:43:17 -0600

    Reverted group id change.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject clojang/zhang "0.1.0-SNAPSHOT"
+(defproject zhang "0.1.0-SNAPSHOT"
   :description "An implementation of the Erlang process model in Clojure
                 core.async"
-  :url "https://github.com/oubiwann/zhang"
+  :url "https://github.com/clojang/zhang"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[clojang/clojang "0.2.0-SNAPSHOT"]
