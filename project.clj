d0e6ee34f1876b19c1262752014f01e55f6bf538
Duncan McGreggor <oubiwann@gmail.com>
2017-01-07 19:36:10 -0600

    Updated deps and fixed version.

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject zhang "0.1.0-dev"
+(defproject zhang "0.1.0-SNAPSHOT"
   :description "An implementation of the Erlang process model in Clojure
                 core.async"
   :url "https://github.com/oubiwann/zhang"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/core.async "0.2.395"]
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [dire "0.5.4"]
                  [potemkin "0.4.3"]
-                 [twig "0.1.2"]]
-  :plugins [[lein-codox "0.9.4"]
+                 [clojusc/twig "0.3.0"]]
+  :plugins [[lein-codox "0.10.2"]
             [lein-simpleton "1.3.0"]]
   :repl-options {:init-ns zhang.dev}
   :test-selectors {:default :unit
