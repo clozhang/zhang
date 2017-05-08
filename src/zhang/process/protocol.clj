a9658a4b87ce611e8295b28f601be671a5cef018
Duncan McGreggor <oubiwann@gmail.com>
2017-05-07 23:19:33 -0500

    Added starting protocol for Zhang processes.

---
+++ src/zhang/process/protocol.clj
@@ -0,0 +1,43 @@
+(ns zhang.process.protocol)
+
+(defprotocol ZhangProcessFactory
+  "Zhang supports creating its processes in the following ways."
+  (spawn
+    [this fun]
+    [this node fun]
+    [this ns fun args]
+    [this node ns fun args]
+    "Runs a function in a light-weight, non-OS/non-JVM thread (the exact
+    mechanism used is up to the implementation). Returns a ZhangProcess.")
+  (spawn!
+    [this fun]
+    [this node fun]
+    [this ns fun args]
+    [this node ns fun args]
+    "Runs a function in separate thread. Returns a ZhangProcess.")
+  (spawn-linked
+    [this]
+    "")
+  (spawn-linked!
+    [this]
+    "")
+  (spawn-managed
+    [this]
+    "")
+  (spawn-managed!
+    [this]
+    ""))
+
+(defprotocol ZhangProcess
+  "Zhang processes support the following "
+  (id
+    [this]
+    "Returns the implementation-specific id, the means by which one process is
+    distinguised from another, or the means by which a process may be
+    communicated.")
+  (!
+    [this msg]
+    "")
+  (receive
+    [this]
+    ""))
